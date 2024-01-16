18d58590f6eb769a7df083d7267e0e5636537e6d
Dhananjay <dhananjayyadav221303@gmail.com>
2024-01-16 21:51:14 +0530

    modified database

--- server/database/db.js
+++ server/database/db.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
 const Connection = async (username, password) => {
-    const URL = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.2';
-    //  const URL = `mongodb://${username}:${password}@blogweb-shard-00-00.ch1hk.mongodb.net:27017,blogweb-shard-00-01.ch1hk.mongodb.net:27017,blogweb-shard-00-02.ch1hk.mongodb.net:27017/BLOG?ssl=true&replicaSet=atlas-lhtsci-shard-0&authSource=admin&retryWrites=true&w=majority`;
-   // const URL = `mongodb://dhananjay:dhananjay@cluster0.dwkvctr.mongodb.net/?retryWrites=true&w=majority`;
-    mongoose.set('strictQuery', false);
+    //const URL = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.2';
+    const URL = `mongodb+srv://dhananjayyadav221303:bloggingsite@cluster0.ffcfpwh.mongodb.net/?retryWrites=true&w=majority`;
+
+  mongoose.set('strictQuery', false);
     try {
         await mongoose.connect(URL, { useNewUrlParser: true })
         console.log('Database connected successfully');
